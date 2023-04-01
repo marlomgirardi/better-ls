d2689fa7dd38892458e4f83e9d4fd34dd250ac8f
Marlom Girardi <marlom.girardi@gmail.com>
2023-04-01 13:41:41 +0100

    feat: throw error on non-unix systems

--- src/main.rs
+++ src/main.rs
@@ -8,13 +8,25 @@ use clap::Parser;
 use std::path::PathBuf;
 
 fn main() {
-    let args = cli::Args::parse();
-    let mut paths: Vec<PathBuf> = args.paths.iter().map(|s| s.into()).collect();
+    #[cfg(unix)]
+    {
+        let args = cli::Args::parse();
+        let mut paths: Vec<PathBuf> = args.paths.iter().map(|s| s.into()).collect();
 
-    if paths.is_empty() {
-        paths.push(cli::get_current_dir());
+        if paths.is_empty() {
+            paths.push(cli::get_current_dir());
+        }
+
+        // TODO: handle multiple paths
+        entry::list_entries(&paths[0], &args);
     }
 
-    // TODO: handle multiple paths
-    entry::list_entries(&paths[0], &args);
+    // TODO: learn a bit more about how the cfg attribute works
+    #[cfg(not(unix))]
+    {
+        Err(io::Error::new(
+            io::ErrorKind::Unsupported,
+            "This program is only supported on Unix systems.",
+        ))
+    }
 }
